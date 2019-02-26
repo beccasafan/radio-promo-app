027d28098cdac387973c71e8d598771f95ece754
beccasafan <beccasafan@gmail.com>
2019-02-26 13:47:37 -0600

    trigger change on the initial value

--- code/client/src/components/app.tsx
+++ code/client/src/components/app.tsx
@@ -25,7 +25,7 @@ export class App extends React.Component<object, AppState> {
 
     componentDidMount() {
         google.script.run.withSuccessHandler((data: CountrySummary[]) => {
-            this.setState({ countries: data });
+            this.setState({ countries: data, selectedCountry: data.find(c => c.code === "US") });
         }).getCountrySummaries();
     }
 
