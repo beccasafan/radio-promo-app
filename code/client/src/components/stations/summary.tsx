d91573e831f9c302acbffbaecce8f62627d607e9
beccasafan <beccasafan@gmail.com>
2019-02-26 22:27:03 -0600

    font-awesome solid style

--- code/client/src/components/stations/summary.tsx
+++ code/client/src/components/stations/summary.tsx
@@ -33,15 +33,15 @@ export class Summary extends React.Component<StationSummaryProps, StationSummary
                     </div>
                     <div className="card-footer">
                         <div className="row">
-                            <div className="col"><a href="#"><i className="far fa-eye"></i></a></div>
-                            <div className="col">{this.props.station.website && <a href={this.props.station.website} target="_blank"><i className="far fa-link"></i></a>}</div>
+                            <div className="col"><a href="#"><i className="fas fa-eye"></i></a></div>
+                            <div className="col">{this.props.station.website && <a href={this.props.station.website} target="_blank"><i className="fas fa-link"></i></a>}</div>
                             <div className="col">{this.props.station.twitter && <a href={`https://twitter.com/${this.props.station.twitter}`} target="_blank"><i className="fab fa-twitter"></i></a>}</div>
                             <div className="col">{this.props.station.instagram && <a href={`https://instagram.com/${this.props.station.instagram}`} target="_blank"><i className="fab fa-instagram"></i></a>}</div>
                             <div className="col">{this.props.station.facebook && <a href={`https://facebook.com/${this.props.station.facebook}`} target="_blank"><i className="fab fa-facebook"></i></a>}</div>
-                            <div className="col">{this.props.station.email && <a href={`mailto:${this.props.station.email}`} target="_blank"><i className="far fa-envelope"></i></a>}</div>
-                            <div className="col">{this.props.station.text && <a href="#"><i className="far fa-comment"></i></a>}</div>
-                            <div className="col">{this.props.station.phone && <a href="#"><i className="far fa-phone"></i></a>}</div>
-                            <div className="col">{this.props.station.note && <a href="#"><i className="far fa-sticky-note"></i></a>}</div>
+                            <div className="col">{this.props.station.email && <a href={`mailto:${this.props.station.email}`} target="_blank"><i className="fas fa-envelope"></i></a>}</div>
+                            <div className="col">{this.props.station.text && <a href="#"><i className="fas fa-comment"></i></a>}</div>
+                            <div className="col">{this.props.station.phone && <a href="#"><i className="fas fa-phone"></i></a>}</div>
+                            <div className="col">{this.props.station.note && <a href="#"><i className="fas fa-sticky-note"></i></a>}</div>
                         </div>
                     </div>
                 </div>
