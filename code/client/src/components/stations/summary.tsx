e5606f26898bc7abfb5502760792978d7c423d17
beccasafan <beccasafan@gmail.com>
2019-02-26 22:08:34 -0600

    grid change

--- code/client/src/components/stations/summary.tsx
+++ code/client/src/components/stations/summary.tsx
@@ -23,18 +23,20 @@ export class Summary extends React.Component<StationSummaryProps, StationSummary
         }
 
         return (
-            <div className="card col-sm-12 col-med-4 col-lg-3">
-                <div className="card-header">{this.props.station.code}</div>
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.station.name}</h5>
-                    <p className="card-text">{this.props.station.location}</p>
-                    {!Util.isEmpty(this.props.station.parentGroup) && <p className="card-text">{this.props.station.parentGroup}</p>}
-                </div>
-                <div className="card-footer">
-                    <div className="row">
-                        <div className="col">{this.props.station.website && <a href={this.props.station.website} target="_blank"><i className="fas fa-link"></i></a>}</div>
-                        <div className="col">{this.props.station.twitter && <a href={this.props.station.twitter} target="_blank"><i className="fab fa-twitter"></i></a>}</div>
-                        <div className="col">{this.props.station.instagram && <a href={this.props.station.instagram} target="_blank"><i className="fab fa-instagram"></i></a>}</div>
+            <div className="col-sm-12 col-med-4 col-lg-3">
+                <div className="card">
+                    <div className="card-header">{this.props.station.code}</div>
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.station.name}</h5>
+                        <p className="card-text">{this.props.station.location}</p>
+                        {!Util.isEmpty(this.props.station.parentGroup) && <p className="card-text">{this.props.station.parentGroup}</p>}
+                    </div>
+                    <div className="card-footer">
+                        <div className="row">
+                            <div className="col">{this.props.station.website && <a href={this.props.station.website} target="_blank"><i className="fas fa-link"></i></a>}</div>
+                            <div className="col">{this.props.station.twitter && <a href={this.props.station.twitter} target="_blank"><i className="fab fa-twitter"></i></a>}</div>
+                            <div className="col">{this.props.station.instagram && <a href={this.props.station.instagram} target="_blank"><i className="fab fa-instagram"></i></a>}</div>
+                        </div>
                     </div>
                 </div>
             </div>
