dcf49e16112a020dfd4f674714b010ed94dea288
beccasafan <beccasafan@gmail.com>
2019-02-26 23:48:24 -0600

    selector scope specificity

--- code/client/src/styles/country.scss.d.ts
+++ code/client/src/styles/country.scss.d.ts
@@ -1,4 +1,5 @@
 // *** Generated File - Do not Edit ***
 export const cardImgOverlay: string;
 export const countrySelector: string;
+export const select2Container: string;
 export const countryDropdownSelect2: string;
