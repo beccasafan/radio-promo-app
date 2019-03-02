ef3611877d4c3f60fde2bc43946a73211a3b2ff7
beccasafan <beccasafan@gmail.com>
2019-03-01 23:53:21 -0600

    using Map and then silencing errors was a really bad idea

--- code/server/util/cache.ts
+++ code/server/util/cache.ts
@@ -1,6 +1,6 @@
 export class CacheWrapper {
     public static ScriptCache = new CacheWrapper(CacheService.getScriptCache());
-    
+
     private static MaxLength: number = 1024 * 90;
     private static CacheDuration: number = 60 * 60 * 6;
 
@@ -10,56 +10,40 @@ export class CacheWrapper {
 
     public put(key: string, value: any): any {
         console.log("cache putting", key);
-        try {
-            var json = JSON.stringify(value);
+        var json = JSON.stringify(value);
 
-            var chunkSize = Math.floor(CacheWrapper.MaxLength / 2);
-            var chunks: Map<string, string> = new Map<string, string>();
-            var index = 0;
-            while (index < json.length) {
-                var chunkKey = `${key}_${index}`;
-                chunks.set(chunkKey, json.substr(index, chunkSize));
-                index += chunkSize;
-            }
-
-            var cacheObject = {};
-            var chunkKeys = [];
-            for ([key, value] of chunks) {
-                cacheObject[key] = value;
-                chunkKeys.push(key);
-            }
-            cacheObject[key] = JSON.stringify(chunkKeys);
-            this.cache.putAll(cacheObject, CacheWrapper.CacheDuration);
-
-            console.log("cache put", Object.keys(cacheObject));
-            return chunkKeys;
-        }
-        catch (e) {
-            // just return the null
+        var chunkSize = Math.floor(CacheWrapper.MaxLength / 2);
+        var chunks = {};
+        var chunkKeys = [];
+        var index = 0;
+        while (index < json.length) {
+            var chunkKey = `${key}_${index}`;
+            chunkKeys.push(chunkKey);
+            chunks[chunkKey] = json.substr(index, chunkSize);
+            index += chunkSize;
         }
 
-        return null;
+        chunks[key] = JSON.stringify(chunkKeys);
+        this.cache.putAll(chunks, CacheWrapper.CacheDuration);
+
+        console.log("cache put", chunkKeys);
+        return chunkKeys;
+
     }
 
     public get<T>(key: string): T {
         console.log("cache getting", key);
-        try {
-            var json = this.cache.get(key);
-            if (json != null) {
-                var chunkKeys = JSON.parse(json);
-                var chunks = this.cache.getAll(chunkKeys);
+        var json = this.cache.get(key);
+        if (json != null) {
+            var chunkKeys = JSON.parse(json);
+            var chunks = this.cache.getAll(chunkKeys);
 
-                var chunkData = chunkKeys.map(chunkKey => chunks[chunkKey]);
-                if (chunkData.every(datum => datum != null)) {
-                    console.log("cache got", chunkKeys);
-                    return JSON.parse(chunkData.join(""));
-                }
+            var chunkData = chunkKeys.map(chunkKey => chunks[chunkKey]);
+            if (chunkData.every(datum => datum != null)) {
+                console.log("cache got", chunkKeys);
+                return JSON.parse(chunkData.join(""));
             }
         }
-        catch (e) {
-            // just return the null
-        }
-        return null;
     }
 
     public remove(key: string): void {
