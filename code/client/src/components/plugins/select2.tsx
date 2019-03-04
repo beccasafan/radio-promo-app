1e8cc8522af4e4f3122453025e95593a3467942c
beccasafan <beccasafan@gmail.com>
2019-03-04 03:35:21 -0600

    clear button handler

--- code/client/src/components/plugins/select2.tsx
+++ code/client/src/components/plugins/select2.tsx
@@ -7,8 +7,8 @@ interface JQuery<TElement = HTMLElement> extends Iterable<TElement> {
 }
 
 export class Select2 extends React.Component<any, object> {
-    private el: HTMLElement;
-    private $el: JQuery<HTMLElement>;
+    private el: HTMLSelectElement;
+    private $el: JQuery<HTMLSelectElement>;
 
     constructor(props: any) {
         super(props);
@@ -19,6 +19,9 @@ export class Select2 extends React.Component<any, object> {
         this.$el = $(this.el);
         var props = Object.assign({}, this.props, { theme: "bootstrap4" });
         this.$el.select2(props);
+        if (props.allowClear === true || props.allowClear === "true") {
+            $("~ .select2-container .select2-selection__clear", this.el).on("click", (e) => { console.log(e); $(this.el).val('').trigger('change'); });
+        }
         Object.keys(this.props.events).forEach(key => $(this.el).on(key, (e) => { this.props.events[key](e) }));
     }
 
