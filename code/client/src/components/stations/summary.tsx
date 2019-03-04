b9dfeaf5e2319842b615d0cb967cf421f0a59405
beccasafan <beccasafan@gmail.com>
2019-03-03 21:59:52 -0600

    temp remove icons

--- code/client/src/components/stations/summary.tsx
+++ code/client/src/components/stations/summary.tsx
@@ -52,19 +52,7 @@ console.log("rendering", this.props.station.code);
                         </div>
                         <a href="javascript:;" onClick={this.open} className="btn btn-outline-secondary btn-block">View</a>
                     </div>
-                    <div className="card-footer">
-                        <div className="row no-gutters">
-                            <div className="col"><a href="javascript:;" onClick={this.open}><i className="fas fa-eye"></i></a></div>
-                            <div className="col">{this.props.station.website && <a href={this.props.station.website} target="_blank"><i className="fas fa-link"></i></a>}</div>
-                            <div className="col">{this.props.station.twitter && <a href={`https://twitter.com/${this.props.station.twitter}`} target="_blank"><i className="fab fa-twitter"></i></a>}</div>
-                            <div className="col">{this.props.station.instagram && <a href={`https://instagram.com/${this.props.station.instagram}`} target="_blank"><i className="fab fa-instagram"></i></a>}</div>
-                            <div className="col">{this.props.station.facebook && <a href={`https://facebook.com/${this.props.station.facebook}`} target="_blank"><i className="fab fa-facebook"></i></a>}</div>
-                            <div className="col">{this.props.station.email && <a href={`mailto:${this.props.station.email}`} target="_blank"><i className="fas fa-envelope"></i></a>}</div>
-                            <div className="col">{this.props.station.text && <a href="javascript:;" onClick={this.open}><i className="fas fa-comment"></i></a>}</div>
-                            <div className="col">{this.props.station.phone && <a href="javascript:;" onClick={this.open}><i className="fas fa-phone"></i></a>}</div>
-                            <div className="col">{this.props.station.note && <a href="javascript:;" onClick={this.open}><i onClick={this.open} className="fas fa-sticky-note"></i></a>}</div>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         )
