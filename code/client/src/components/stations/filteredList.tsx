b0f647f31bf02fabd951e106f6840d1832cea94c
beccasafan <beccasafan@gmail.com>
2019-03-04 21:50:10 -0600

    what are you doing react?

--- code/client/src/components/stations/filteredList.tsx
+++ code/client/src/components/stations/filteredList.tsx
@@ -21,6 +21,7 @@ export class FilteredList extends React.Component<FilteredListProps, FilteredLis
     tweetGenerator: TweetGenerator;
     constructor(props: FilteredListProps) {
         super(props);
+        console.log("creating new Filtered List", this.props.countryId, this.props.tweets);
 
         this.state = {};
 
