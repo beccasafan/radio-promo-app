2d5de53b015615677a6040f548b82532c88a7287
beccasafan <beccasafan@gmail.com>
2019-03-05 03:11:28 -0600

    perhaps this loading...

--- code/client/src/components/app.tsx
+++ code/client/src/components/app.tsx
@@ -116,6 +116,8 @@ export class App extends React.Component<object, AppState> {
                         </div>
                     )}
 
+                    {this.state.countries && !(this.state.stations || this.state.tweets) && <p>Loading...</p>}
+
                     {this.state.countries && this.state.selectedCountry && this.state.tweets && <Stations key={`${this.state.selectedCountry.id}_${Object.keys(this.state.tweets).join(",")}`} countryId={this.state.selectedCountry.id} stations={this.state.stations} search={this.state.search} onSelect={this.stationSelected} getTweetUrl={this.getTweetUrl} />}
 
                     {this.state.countries && this.state.stations && this.state.selectedStation && <Detail station={this.state.selectedStation} detail={this.state.selectedStationDetails} handleClose={this.stationUnselected} getTweetUrl={this.getTweetUrl} />}
