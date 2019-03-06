7e6d11bb46b51de40d0330e3ce19ae2dd789baa3
beccasafan <beccasafan@gmail.com>
2019-03-05 18:39:48 -0600

    tweets guard

--- code/client/src/components/tweetGenerator.ts
+++ code/client/src/components/tweetGenerator.ts
@@ -13,7 +13,7 @@ export class TweetGenerator {
     }
 
     get(languageId: string, target: string) {
-        if (this.tweets == null || this.tweets[languageId] == null) {
+        if (this.tweets == null || this.tweets[languageId] == null || this.tweets[languageId].length <= 0) {
             return "";
         }
 
