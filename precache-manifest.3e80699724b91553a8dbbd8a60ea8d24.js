e84895e596cb93895a35db11cabf26fbeaef34dd
beccasafan <beccasafan@gmail.com>
2019-08-27 22:41:50 -0500

    Updates

---
+++ precache-manifest.3e80699724b91553a8dbbd8a60ea8d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89bdeb4196560c90fe4f",
+    "url": "/radio-promo-app/static/js/runtime~main.9db086f4.js"
+  },
+  {
+    "revision": "9cda63e3dbe719b4149c",
+    "url": "/radio-promo-app/static/js/main.da8ada67.chunk.js"
+  },
+  {
+    "revision": "4aba4d8ff93f2a43736a",
+    "url": "/radio-promo-app/static/js/2.650375fe.chunk.js"
+  },
+  {
+    "revision": "9cda63e3dbe719b4149c",
+    "url": "/radio-promo-app/static/css/main.ee21029d.chunk.css"
+  },
+  {
+    "revision": "4aba4d8ff93f2a43736a",
+    "url": "/radio-promo-app/static/css/2.1baa7fd0.chunk.css"
+  },
+  {
+    "revision": "8d42772a369d4e551f463a31dc734728",
+    "url": "/radio-promo-app/index.html"
+  }
+];
